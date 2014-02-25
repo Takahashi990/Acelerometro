b5fc86f5a5b976e34d321362bec93a6b8bba3f1e
Victor Takahashi <victor.takahashi@uol.com.br>
2014-02-25 17:13:50 -0300

    Signed-off-by: Victor Takahashi <victor.takahashi@uol.com.br>

---
+++ Debug/main.pp
@@ -0,0 +1,15 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti3/ccsv5/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti3/ccsv5/ccs_base/msp430/include/msp430f2274.h
+main.obj: C:/ti3/ccsv5/ccs_base/msp430/include/in430.h
+main.obj: C:/ti3/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h
+main.obj: C:/ti3/grace_2_20_01_12/packages/ti/mcu/msp430/Grace.h
+
+../main.c: 
+C:/ti3/ccsv5/ccs_base/msp430/include/msp430.h: 
+C:/ti3/ccsv5/ccs_base/msp430/include/msp430f2274.h: 
+C:/ti3/ccsv5/ccs_base/msp430/include/in430.h: 
+C:/ti3/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h: 
+C:/ti3/grace_2_20_01_12/packages/ti/mcu/msp430/Grace.h: 
